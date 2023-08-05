decdb933f8f1aed7cc89ede213cb1ef8de376394
Pavel Tchoursinov <eastbalt.ru@gmail.com>
2023-08-05 18:50:04 +0300

    3.01 Учтены замечания как с учебной платформы, так и оставленные внутри ПР.
    Возможно требования к этой работе забегают вперед, в спринт 8 (стили, шрифты):
    
    1) "не свитч сливается с фоном" - понять сложно. Выставил точные значения цветов для thumb, trail - как в Фигме. В активном состоянии макет не показывает ничего.
    
    2) Шрифты:
    "26.07.2023 Алексей Анисимов
    12:09
    Друзья, всем привет!
    шрифты нужны будут только в 9ом спринте
    обсудил этот момент с ревьюверами, мы обновили критерии приёмки 8го спринта, там не будет проверки шрифтов".

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 /*
-        val searchButton = findViewById<View>(R.id.search)
+        val searchButton = findViewById<View>(R.id.buttonSearch)
         val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
             override fun onClick(v: View?) {
                 Toast.makeText(this@MainActivity, "Нажали на кнопку SEARCH", Toast.LENGTH_SHORT).show()
@@ -20,20 +19,20 @@ class MainActivity : AppCompatActivity() {
         searchButton.setOnClickListener(searchButtonClickListener)
  */
 
-        val searchButton = findViewById<View>(R.id.search)
+        val searchButton = findViewById<View>(R.id.buttonSearch)
         searchButton.setOnClickListener {
             val searchIntent = Intent(this, SearchActivity::class.java)
             startActivity(searchIntent)
         }
 
-        val mediaButton = findViewById<View>(R.id.media)
+        val mediaButton = findViewById<View>(R.id.buttonMedia)
         mediaButton.setOnClickListener {
             val mediaIntent = Intent(this, MediaActivity::class.java)
             startActivity(mediaIntent)
  //           Toast.makeText(this@MainActivity, "Нажали на кнопку MEDIA", Toast.LENGTH_SHORT).show()
         }
 
-        val settingsButton = findViewById<View>(R.id.settings)
+        val settingsButton = findViewById<View>(R.id.buttonSettings)
         settingsButton.setOnClickListener {
             val settingsIntent = Intent(this, SettingsActivity::class.java)
             startActivity(settingsIntent)
