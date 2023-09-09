8081ae9fad90c1aedd4391804b5fd73bb9a89bc8
Pavel Tchoursinov <eastbalt.ru@gmail.com>
2023-09-09 15:47:18 +0300

    sprint 9 topic 2 home work release 1.0

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,10 +12,38 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val settingsBackButton = findViewById<View>(R.id.settings_back)
-        /* settingsBackButton.setOnClickListener {
-            val settingsBackIntent = Intent(this, MainActivity::class.java)
-            startActivity(settingsBackIntent)
-        } */
         settingsBackButton.setOnClickListener { finish() }
+
+        // Кнопка поделиться приложением
+        val settingsShareButton = findViewById<View>(R.id.settings_share)
+        settingsShareButton.setOnClickListener {
+            val settingsShareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/android-developer/")
+                type = "text/plain"
+            }
+            startActivity(settingsShareIntent)
+        }
+
+        // Кнопка написать в поддержку
+        val settingsCallSupportButton = findViewById<View>(R.id.settings_call_support)
+        settingsCallSupportButton.setOnClickListener {
+            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val settingsCallSupportIntent = Intent(Intent.ACTION_SENDTO)
+            settingsCallSupportIntent.data = Uri.parse("mailto:")
+            settingsCallSupportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("monitor1010@yandex.ru"))
+            settingsCallSupportIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            settingsCallSupportIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(settingsCallSupportIntent)
+        }
+
+        // Кнопка пользовательское соглашение
+        val settingsUserAgreementButton = findViewById<View>(R.id.settings_user_agreement)
+        settingsUserAgreementButton.setOnClickListener {
+            val url = Uri.parse("https://yandex.ru/legal/practicum_termsofuse/")
+            val settingsUserAgreementintent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(settingsUserAgreementintent)
+        }
     }
 }
