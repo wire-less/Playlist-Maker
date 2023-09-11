f35caffa3b478b162ebde65cd7e45103e2c958bc
Pavel Tchoursinov <eastbalt.ru@gmail.com>
2023-09-11 23:39:09 +0300

    sprint 9 topic 3 home work release 1.3

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,10 +2,62 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-    }
+
+        val activitySearchLinearLayout = findViewById<LinearLayout>(R.id.container_search)
+        val activitySearchEditText = findViewById<EditText>(R.id.activitySearchEditText)
+        val activitySearchClearIcon = findViewById<ImageView>(R.id.activitySearchClearIcon)
+
+        activitySearchClearIcon.setOnClickListener {
+            activitySearchEditText.setText("")
+        }
+
+
+val activitySearchTextWatcher = object : TextWatcher {
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    // empty
 }
+
+override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+    if (s.isNullOrEmpty()) {
+    // do something
+} else {
+val input = s.toString()
+}
+    activitySearchClearIcon.visibility = clearButtonVisibility(s)
+}
+
+override fun afterTextChanged(s: Editable?) {
+    // empty
+}
+}
+// business if here
+activitySearchEditText.addTextChangedListener(activitySearchTextWatcher)
+}
+
+private fun clearButtonVisibility(s: CharSequence?): Int {
+return if (s.isNullOrEmpty()) {
+View.GONE
+} else {
+View.VISIBLE
+}
+}
+
+}
+
+
+/*
+//Код ниже может скрыть клавиатуру:
+val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+inputMethodManager?.hideSoftInputFromWindow(`currentView`.windowToken, 0)
+ */
